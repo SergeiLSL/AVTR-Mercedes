a056d97e5933f8d131f94b5ec2f2b03afd24a14e
Sergei Student <lsl1959@mail.ru>
2022-09-21 21:41:41 +0600

    верстаю секцию interior

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -19,9 +19,9 @@ EffectFade, Lazy, Manipulation
 // Базовые стили
 import "../../scss/base/swiper.scss";
 // Полный набор стилей из scss/libs/swiper.scss
-// import "../../scss/libs/swiper.scss";
+import "../../scss/libs/swiper.scss";
 // Полный набор стилей из node_modules
-// import 'swiper/css';
+//import 'swiper/css';
 
 // Инициализация слайдеров
 function initSliders() {
@@ -35,14 +35,14 @@ function initSliders() {
 			modules: [Navigation],
 			observer: true,
 			observeParents: true,
-			slidesPerView: 1,
+			slidesPerView: 4,
 			spaceBetween: 0,
 			autoHeight: true,
 			speed: 800,
 
 			//touchRatio: 0,
 			//simulateTouch: false,
-			//loop: true,
+			loop: true,
 			//preloadImages: false,
 			//lazy: true,
 
